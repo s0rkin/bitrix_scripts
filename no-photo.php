d47ad8f0ee701e3ab98c01417576f1fef7c0b107
s0rry <firebat2@mail.ru>
2023-11-18 15:09:47 +0300

    Update no-photo.php

--- no-photo.php
+++ no-photo.php
@@ -5,8 +5,8 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.ph
 CModule::IncludeModule('iblock');
 
 $quantity = 0;
-$iblock_id = 10; #ID инфоблока с товарами
-$site = "https://site.ru" #URL сайта
+$iblock_id = 15;
+$site = "https://site.ru";
 
 $arSelect = Array("IBLOCK_ID","SECTION_ACTIVE", "ID");
 $arFilter = Array("IBLOCK_ID"=>$iblock_id,"SECTION_ACTIVE" => "Y", "PREVIEW_PICTURE" => false, "DETAIL_PICTURE" => false);
@@ -36,11 +36,11 @@ while($ob = $res->Fetch())
 	<h1><? echo 'Всего элементов без фото в каталоге - '.$quantity;?></h1>
 	<ol>
 	<?foreach ($arExternal_ID as $it => $arTab):?>
-		<li><pre><a href="<?=$arTab['URL']?>" target="blank"><?=$arTab['NAME']?></a><text> - ID Товара: <?=$arTab['ID']?>. Товар активен? - <?=$arTab["ACTIVE"]?> ***<a href=""$site . "/bitrix/admin/iblock_element_edit.php?IBLOCK_ID=" . $iblock_id . "&type=catalog&lang=ru&ID=<?=$arTab['ID']?>" target="blank">Ссылка в админку</a></text></pre></li>
+		<li><pre><a href="<?=$arTab['URL']?>" target="blank"><?=$arTab['NAME']?></a><text> - ID Товара: <?=$arTab['ID']?>. Товар активен? - <?=$arTab["ACTIVE"]?> ***<a href="<?=$site?>/bitrix/admin/iblock_element_edit.php?IBLOCK_ID=<?=$iblock_id?>&type=catalog&lang=ru&ID=<?=$arTab['ID']?>" target="blank">Ссылка в админку</a></text></pre></li>
 	<?endforeach;?>
 	</ol>
 	<?else:?>
-	<text>brrrr....</text>
+	<text>NOTHING</text>
 	<?endif?>
 </div>
 </body>
